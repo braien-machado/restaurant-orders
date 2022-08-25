86d27ea8578a7e9e313f946bf76ef720f86191bb
Braien Machado de Paula <braienmp@outlook.com>
2022-08-25 20:42:53 -0300

    chore: handle errors in analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,12 @@
+import csv
+import re
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not re.search('csv$', path_to_file):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file, encoding = 'utf-8') as file:
+            print(path_to_file)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
