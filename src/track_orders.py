3836cbe35ce26b3f494c357a3bdcb39bc2d34780
Braien Machado de Paula <braienmp@outlook.com>
2022-08-26 20:52:06 -0300

    feat: track_orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,41 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
     # aqui deve expor a quantidade de estoque
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
+
+    def get_orders_per_customer(self, customer):
+        return [order for order in self.orders if order[0] == customer]
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        customer_orders = self \
+            .get_orders_per_customer(customer)
+        return Counter([order[1] for order in customer_orders]) \
+            .most_common()[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_dishes = {order[1] for order in self.orders}
+        customer_orders = self.get_orders_per_customer(customer)
+        customer_dishes = {order[1] for order in customer_orders}
+        return all_dishes.difference(customer_dishes)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        restaurant_days_opened = {order[2] for order in self.orders}
+        customer_orders = self.get_orders_per_customer(customer)
+        client_days = {order[2] for order in customer_orders}
+        return restaurant_days_opened.difference(client_days)
 
     def get_busiest_day(self):
-        pass
+        return Counter([order[2] for order in self.orders]).most_common()[0][0]
 
     def get_least_busy_day(self):
-        pass
+        return Counter([order[2] for order in self.orders]) \
+            .most_common()[-1][0]
