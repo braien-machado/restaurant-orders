b850a3a249cc702c11bee64a6517241e5361b687
Braien Machado de Paula <braienmp@outlook.com>
2022-08-25 23:05:36 -0300

    refactor & feat: analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,28 +1,80 @@
+from collections import Counter
 import csv
 import re
-from .utils.most_ordered_dish import (
-    get_most_ordered_dish_by_customer,
-    get_customer_order_quantity_by_dish
-)
 
 
-def analyze_log(path_to_file):
-    if not re.search('csv$', path_to_file):
+def get_orders_by_customer(orders, customer_name):
+    return [order for order in orders if order[0] == customer_name]
+
+
+def most_ordered_dish_by_customer(orders, customer_name):
+    customer_orders = get_orders_by_customer(orders, customer_name)
+    return Counter([order[1] for order in customer_orders]).most_common()[0][0]
+
+
+def customer_order_quantity_by_dish(orders, customer_name, dish_name):
+    customer_orders = get_orders_by_customer(orders, customer_name)
+    dishes_quantity = Counter([order[1] for order in customer_orders])
+
+    for dish in dishes_quantity:
+        if dish_name in dish:
+            return dishes_quantity[dish]
+
+    return 0
+
+
+def never_ordered_dishes_by_customer(orders, customer_name):
+    all_dishes = {order[1] for order in orders}
+    customer_orders = get_orders_by_customer(orders, customer_name)
+    customer_dishes = {order[1] for order in customer_orders}
+
+    return all_dishes.difference(customer_dishes)
+
+
+def days_customer_never_went(orders, customer_name):
+    restaurant_days_opened = {order[2] for order in orders}
+    customer_orders = get_orders_by_customer(orders, customer_name)
+    client_days = {order[2] for order in customer_orders}
+
+    return restaurant_days_opened.difference(client_days)
+
+
+
+def csv_reader(path_to_file):
+    if 'csv' not in path_to_file:
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
     try:
         with open(path_to_file, encoding='utf-8') as file:
-            orders_list = list(csv.reader(file, delimiter=',', quotechar='"'))
-            maria_most_ordered_dish = get_most_ordered_dish_by_customer(
-                orders_list,
-                'maria'
-            )
-            arnaldo_hamburguer_orders = get_customer_order_quantity_by_dish(
-                orders_list,
-                'arnaldo',
-                'hamburguer'
-            )
-            print(maria_most_ordered_dish)
-            print(arnaldo_hamburguer_orders)
-
+            return list(csv.reader(file, delimiter=',', quotechar='"'))
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+
+def analyze_log(path_to_file):
+    orders_list = csv_reader(path_to_file)
+    maria_most_ordered_dish = most_ordered_dish_by_customer(
+        orders_list,
+        'maria'
+    )
+    arnaldo_hamburguer_orders = customer_order_quantity_by_dish(
+        orders_list,
+        'arnaldo',
+        'hamburguer'
+    )
+    joao_never_ordered_dishes = never_ordered_dishes_by_customer(
+        orders_list,
+        'joao'
+    )
+    days_joao_never_appeared = days_customer_never_went(
+        orders_list,
+        'joao'
+    )
+
+    file = open('data/mkt_campaign.txt', mode='w')
+    LINES = [
+        f'{maria_most_ordered_dish}\n',
+        f'{arnaldo_hamburguer_orders}\n',
+        f'{joao_never_ordered_dishes}\n',
+        f'{days_joao_never_appeared}\n',
+    ]
+    file.writelines(LINES)
