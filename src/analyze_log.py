fe8a7709e2522afbd3bc215bac46fe79cdae8109
Braien Machado de Paula <braienmp@outlook.com>
2022-08-25 21:54:09 -0300

    chore: get arnaldo hamburguer orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,9 @@
 import csv
 import re
-from .utils.most_ordered_dish import get_most_ordered_dish_by_customer
+from .utils.most_ordered_dish import (
+    get_most_ordered_dish_by_customer,
+    get_customer_order_quantity_by_dish
+)
 
 
 def analyze_log(path_to_file):
@@ -8,11 +11,18 @@ def analyze_log(path_to_file):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
     try:
         with open(path_to_file, encoding='utf-8') as file:
-            orders_reader = csv.reader(file, delimiter=',', quotechar='"')
+            orders_list = list(csv.reader(file, delimiter=',', quotechar='"'))
             maria_most_ordered_dish = get_most_ordered_dish_by_customer(
-                orders_reader,
+                orders_list,
                 'maria'
             )
+            arnaldo_hamburguer_orders = get_customer_order_quantity_by_dish(
+                orders_list,
+                'arnaldo',
+                'hamburguer'
+            )
+            print(maria_most_ordered_dish)
+            print(arnaldo_hamburguer_orders)
 
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
--- src/utils/most_ordered_dish.py
+++ src/utils/most_ordered_dish.py
@@ -1,3 +1,22 @@
+def get_orders_by_customer(orders, customer_name):
+    return [
+        order
+        for order in orders
+        if order[0] == customer_name
+    ]
+
+
+def get_list_values_quantity(orders):
+    values_quantity = {}
+    for order in orders:
+        if order[1] not in values_quantity:
+            values_quantity[order[1]] = 1
+        else:
+            values_quantity[order[1]] += 1
+
+    return values_quantity
+
+
 def get_key_by_bigger_value(dict: dict):
     key_and_bigger_value = ['', 0]
     for key, value in dict.items():
@@ -8,11 +27,17 @@ def get_key_by_bigger_value(dict: dict):
 
 
 def get_most_ordered_dish_by_customer(orders, customer_name):
-    dishes_quantity = {}
-    for order in orders:
-        if order[0] == customer_name and order[1] not in dishes_quantity:
-            dishes_quantity[order[1]] = 1
-        elif order[0] == customer_name and order[1] in dishes_quantity:
-            dishes_quantity[order[1]] += 1
+    customer_orders = get_orders_by_customer(orders, customer_name)
+    dishes_quantity = get_list_values_quantity(customer_orders)
 
     return get_key_by_bigger_value(dishes_quantity)[0]
+
+
+def get_customer_order_quantity_by_dish(orders, customer_name, dish_name):
+    customer_orders = get_orders_by_customer(orders, customer_name)
+    dishes_quantity = get_list_values_quantity(customer_orders)
+
+    if dish_name in dishes_quantity:
+        return dishes_quantity[dish_name]
+
+    return 0
